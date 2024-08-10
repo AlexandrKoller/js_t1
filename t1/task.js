a5d02c52d2e662a52e578d2fc95677e613358ce1
Alexandr Koller <wilhelmwigt@yandex.ru>
2024-08-10 17:02:20 +0500

    Final commit

--- t1/task.js
+++ t1/task.js
@@ -1,10 +1,11 @@
 const p = document.getElementById('timer')
-setInterval(()=>{
+let timerId = setInterval(()=>{
     if (Number(p.textContent) == 0) {
         let b = confirm("Вы победили в конкурсе!")
+        clearInterval(timerId)
         if (b === true) {
             document.location.assign("https://netology.ru/")
         }
     }
     p.textContent = Number(p.textContent) - 1
-}, 1000)
+    }, 1000)
